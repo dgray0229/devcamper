14cca5399c7c29ec2f4bd992f5435b005862916b
Devin Gray <admin@devingray.io>
2023-08-01 19:19:00 -0700

    Add custom select and sorting query. Add radius search.

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,13 +1,45 @@
-const Bootcamp = require("../models/Bootcamp");
-const asyncHandler = require("../middleware/async");
 const ErrorResponse = require("../utils/errorResponse");
+const asyncHandler = require("../middleware/async");
+const geocoder = require("../utils/geocoder");
+const Bootcamp = require("../models/Bootcamp");
 // @desc    Get all bootcamps
 // @route   GET /api/v1/bootcamps
 // @access  Public
 exports.getBootcamps = asyncHandler(async (req, res, next) => {
 	try {
 		console.log("show all bootcamps");
-		const bootcamps = await Bootcamp.find();
+		let query;
+    // Copy req.query into a new object to manipulate
+    const reqQuery = { ...req.query };
+    
+    // Fields to exclude
+    const removeFields = ["select", "sort", "page", "limit"];Ï
+
+    // Loop over removeFields and delete them from reqQuery
+    removeFields.forEach((field) => delete reqQuery[field]);
+    
+    // Create query string
+		let queryStr = JSON.stringify(reqQuery);
+    // Create operators ($gt, $gte, etc)
+		queryStr = queryStr.replace(/\b(gt|gte|lt|lte|in)\b/g, (match) => `$${match}`);
+    // Finding resource
+		query = Bootcamp.find(JSON.parse(queryStr));
+
+    // Select Fields
+    if (req.query.select) {
+      const fields = req.query.select.split(',').join(' ');
+      query = query.select(fields);
+    }
+		// Sorting
+		if (req.query.sort) {
+				const sortBy = req.query.sort;
+				query = query.sort(sortBy);
+		} else {
+				query = query.sort({ createdAt: -1 });
+		}
+    // Executing query
+		const bootcamps = await query;
+
 		res.status(200).json({
 			success: true,
 			count: bootcamps.length,
@@ -84,6 +116,33 @@ exports.updateBootcamp = asyncHandler(async (req, res, next) => {
 		res.status(400).json({ success: false, msg: err.message });
 		next(err);
 	}
+});
+
+// @desc	Get bootcamps within a radius
+// @route	GET /api/v1/bootcamps/radius/:zipcode/:distance
+// @access	Private
+exports.getBootcampsInRadius = asyncHandler(async (req, res, next) => {
+	const { zipcode, distance } = req.params;
+
+	// Get lat/lng from geocoder
+	const loc = await geocoder.geocode(zipcode);
+	const lat = loc[0].latitude;
+	const lng = loc[0].longitude;
+
+	// Calc radius using radians
+	// Divide dist by radius of Earth (3,963 mi / 6,378 km)
+	const radius = distance / 3963;
+	const bootcamps = await Bootcamp.find({
+		location: {
+			$geoWithin: { $centerSphere: [[lng, lat], radius] },
+		},
+	});
+	res.status(200).json({
+		success: true,
+		count: bootcamps.length,
+		data: bootcamps,
+		msg: `Show bootcamps within ${distance} miles of ${zipcode}`,
+	});
 });
 
 // @desc    Delete bootcamp
